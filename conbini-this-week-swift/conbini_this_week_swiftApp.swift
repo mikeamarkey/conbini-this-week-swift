822539c420b1f7271a534e94ddeca3ef8b024c62
Michael Markey <michaelamarkey@gmail.com>
2023-06-24 08:19:00 +0900

    Initial Commit

---
+++ conbini-this-week-swift/conbini_this_week_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  conbini_this_week_swiftApp.swift
+//  conbini-this-week-swift
+//
+//  Created by Michael Markey on 2023/06/24.
+//
+
+import SwiftUI
+
+@main
+struct conbini_this_week_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
